27f0c14a36c81ab5e56f383e5d17f2768db2389e
Aryan <aryanjain2462005@gmail.com>
2024-03-24 21:32:25 +0530

    Verufucation prtal added named as Scanner.js

--- screening/src/App.js
+++ screening/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 import { Foram } from "./components/Foram.js";
-
+import {Scanner} from"./components/Scanner.js";
 function App() {
   return (
     <div>
