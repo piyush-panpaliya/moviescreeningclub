2e82a4470983858105c9651515fb400aa883950e
Aryan <aryanjain2462005@gmail.com>
2024-04-11 21:43:21 +0530

    trying to reolve merge conflict

--- screening/src/components/home.js
+++ screening/src/components/home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./home.css";
 import Logo from '../images/logo.png';
 
 const Home = () => {
@@ -20,72 +19,63 @@ const Home = () => {
   // Filter ongoing and upcoming movies
   const ongoingMovies = movies.filter(movie => movie.currentscreening);
   const upcomingMovies = movies.filter(movie => !movie.currentscreening).slice(0, 4); // Display up to 4 upcoming movies
-  console.log(ongoingMovies);
 
   return (
     <>
       <div>
-        <nav className="fnav">
-          <div className="left">
-            <img src="logo.png" alt="" />
-          </div>
-          <div className="right">
-            <i className="fa-solid fa-bars"></i>
-          </div>
-        </nav>
+       
 
-        <div className="container">
-          <h2>Ongoing Movies</h2>
-          <div className="overflow-x-scroll">
-            <div className="movie-list">
-              {ongoingMovies.map((movie) => (
-                <Link
-                  to={`/showtime/${movie._id}`}
-                  key={movie._id}
-                  onClick={() => console.log('Clicked movie ID:', movie._id)} // Add onClick event handler
-                >
-                  <div className="movie-card">
-                    <img src={movie.poster} alt={movie.title} />
-                    <div className="movie-details">
-                      <p className="movie-title">{movie.title}</p>
-                      <p className="movie-genre">{movie.genre}</p>
+        <div className="container mx-auto px-4 py-8">
+          <h2 className="text-2xl font-bold mb-4">Ongoing Movies</h2>
+          <div className="flex overflow-x-auto">
+            {ongoingMovies.map((movie) => (
+              <Link
+                to={`/showtime/${movie._id}`}
+                key={movie._id}
+                onClick={() => console.log('Clicked movie ID:', movie._id)} // Add onClick event handler
+                className="flex-shrink-0 w-64 mr-4"
+              >
+                <div className="bg-gray-100 p-4 rounded-md shadow-md">
+                  <img className="w-full mb-2 rounded" src={movie.poster} alt={movie.title} />
+                  <div>
+                    <p className="text-lg font-semibold">{movie.title}</p>
+                    <p className="text-gray-600">{movie.genre}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          <hr className="my-8 border-t" />
+
+          <div className="container mx-auto">
+            <h2 className="text-2xl font-bold mb-4">Upcoming Movies</h2>
+            <div className="flex overflow-x-auto">
+              {upcomingMovies.map((movie) => (
+                <Link to={`/showtime/${movie._id}`} key={movie._id} className="flex-shrink-0 w-64 mr-4">
+                  <div className="bg-gray-100 p-4 rounded-md shadow-md">
+                    <img className="w-full mb-2 rounded" src={movie.poster} alt={movie.title} />
+                    <div>
+                      <p className="text-lg font-semibold">{movie.title}</p>
+                      <p className="text-gray-600">{movie.genre}</p>
                     </div>
                   </div>
                 </Link>
               ))}
             </div>
           </div>
-
-          <hr className="separator" />
-
-          <div className="container">
-            <h2>Upcoming Movies</h2>
-            <div className="overflow-x-scroll">
-              <div className="movie-list">
-                {upcomingMovies.map((movie) => (
-                  <Link to={`/showtime/${movie._id}`} key={movie._id}>
-                    <div className="movie-card"> {/* movie-card div should be inside the Link */}
-                      <img src={movie.poster} alt={movie.title} />
-                      <div className="movie-details">
-                        <p className="movie-title">{movie.title}</p>
-                        <p className="movie-genre">{movie.genre}</p>
-                      </div>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 
-      <div className="footer">
-        <img src="logo.png" alt="" />
-        <div className="icon">
-          <i className="fa-brands fa-facebook"></i>
-          <i className="fa-brands fa-square-instagram"></i>
-          <i className="fa-brands fa-twitter"></i>
-          <i className="fa-brands fa-youtube"></i>
+      <div className="footer bg-gray-800 text-white py-8">
+        <div className="container mx-auto flex justify-between items-center">
+          <img className="h-8 w-auto" src={Logo} alt="Movies" />
+          <div className="flex space-x-4">
+            <i className="fab fa-facebook"></i>
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-twitter"></i>
+            <i className="fab fa-youtube"></i>
+          </div>
         </div>
       </div>
     </>
