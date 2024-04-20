195230706b479e551ffae027f8c7269afd76e66c
Lord-Morpheus <tarurak4@gmail.com>
2024-04-21 03:51:43 +0530

    updated

--- screening/src/components/home.js
+++ screening/src/components/home.js
@@ -42,82 +42,80 @@ const Home = () => {
   };
 
   return (
-    <>
-      <div>
-        <div className="bg-gray-200 flex flex-col items-center">
-          <div className="flex flex-col bg-white h-1/2 w-4/5 my-10 rounded-xl shadow-lg">
-            <div className="flex justify-between my-3 mx-5">
-              <h2 className="text-xl font-semibold">Ongoing Movies</h2>
-              <p
-                className="font-inter cursor-pointer"
-                onClick={toggleShowMoreOngoing}
-              >
-                {showMoreOngoing ? "Show Less" : "Show More"}
-              </p>
-            </div>
-            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mx-5">
-              {ongoingMovies.map((movie) => (
-                <Link
-                  to={`/showtime/${movie._id}/${encodePosterUrl(movie.poster)}`} // Encode poster URL
-                  key={movie._id}
-                  onClick={() => console.log("Clicked movie ID:", movie._id)} // Add onClick event handler
-                  className="flex items-center justify-center"
-                >
-                  <div className="w-full h-full object-cover">
-                    <div className="w-full h-4/5">
-                      <img
-                        className="object-cover w-full h-full rounded-md"
-                        src={movie.poster}
-                        alt={movie.title}
-                      />
-                    </div>
-                    <div className="flex flex-col mt-1">
-                      <p className="font-semibold text-2xl">{movie.title}</p>
-                      <p className="movie-genre">{movie.genre}</p>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
+    <div >
+      <div className="bg-gray-200 flex flex-col items-center">
+        <div className="flex flex-col bg-white h-1/2 w-4/5 my-10 rounded-xl shadow-lg">
+          <div className="flex justify-between my-3 mx-5">
+            <h2 className="text-xl font-semibold">Ongoing Movies</h2>
+            <p
+              className="font-inter cursor-pointer"
+              onClick={toggleShowMoreOngoing}
+            >
+              {showMoreOngoing ? "Show Less" : "Show More"}
+            </p>
           </div>
-          <div className="flex flex-col bg-white h-1/2 w-4/5 my-10 rounded-xl shadow-lg">
-            <div className="flex justify-between my-3 mx-5">
-              <h2 className="text-xl font-semibold">Upcoming Movies</h2>
-              <p
-                className="font-inter cursor-pointer"
-                onClick={toggleShowMoreUpcoming}
+          <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mx-5">
+            {ongoingMovies.map((movie) => (
+              <Link
+                to={`/showtime/${movie._id}/${encodePosterUrl(movie.poster)}`} // Encode poster URL
+                key={movie._id}
+                onClick={() => console.log("Clicked movie ID:", movie._id)} // Add onClick event handler
+                className="flex items-center justify-center"
               >
-                {showMoreUpcoming ? "Show Less" : "Show More"}
-              </p>
-            </div>
-            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mx-5">
-              {upcomingMovies.map((movie) => (
-                <Link
-                  to={`/showtime/${movie._id}/${encodePosterUrl(movie.poster)}`} // Encode poster URL
-                  key={movie._id}
-                  onClick={() => console.log("Clicked movie ID:", movie._id)} // Add onClick event handler
-                  className="flex items-center justify-center"
-                >
-                  <div className="w-full h-full object-cover">
-                    <div className="w-full h-4/5">
-                      <img
-                        className="object-cover w-full h-full rounded-md"
-                        src={movie.poster}
-                        alt={movie.title}
-                      />
-                    </div>
-                    <div className="flex flex-col mt-1">
-                      <p className="font-semibold text-2xl">{movie.title}</p>
-                      <p className="movie-genre">{movie.genre}</p>
-                    </div>
+                <div className="w-full h-full object-cover">
+                  <div className="w-full h-4/5">
+                    <img
+                      className="object-cover w-full h-full rounded-md"
+                      src={movie.poster}
+                      alt={movie.title}
+                    />
                   </div>
-                </Link>
-              ))}
-            </div>
+                  <div className="flex flex-col mt-1">
+                    <p className="font-semibold text-2xl">{movie.title}</p>
+                    <p className="movie-genre">{movie.genre}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col bg-white h-1/2 w-4/5 my-10 rounded-xl shadow-lg">
+          <div className="flex justify-between my-3 mx-5">
+            <h2 className="text-xl font-semibold">Upcoming Movies</h2>
+            <p
+              className="font-inter cursor-pointer"
+              onClick={toggleShowMoreUpcoming}
+            >
+              {showMoreUpcoming ? "Show Less" : "Show More"}
+            </p>
+          </div>
+          <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mx-5">
+            {upcomingMovies.map((movie) => (
+              <Link
+                to={`/showtime/${movie._id}/${encodePosterUrl(movie.poster)}`} // Encode poster URL
+                key={movie._id}
+                onClick={() => console.log("Clicked movie ID:", movie._id)} // Add onClick event handler
+                className="flex items-center justify-center"
+              >
+                <div className="w-full h-full object-cover">
+                  <div className="w-full h-4/5">
+                    <img
+                      className="object-cover w-full h-full rounded-md"
+                      src={movie.poster}
+                      alt={movie.title}
+                    />
+                  </div>
+                  <div className="flex flex-col mt-1">
+                    <p className="font-semibold text-2xl">{movie.title}</p>
+                    <p className="movie-genre">{movie.genre}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
