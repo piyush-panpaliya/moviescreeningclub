342d3ba21f45a7584f3252fd1ea5920c13be1223
Lord-Morpheus <tarurak4@gmail.com>
2024-04-25 15:32:51 +0530

    seatmap ui prob solved

--- screening/src/components/SeatMap.js
+++ screening/src/components/SeatMap.js
@@ -21,7 +21,7 @@ const SeatMapPage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const userType = localStorage.getItem('userType');
+    const userType = localStorage.getItem("userType");
     if (!userType) {
       navigate("/");
     }
@@ -30,12 +30,13 @@ const SeatMapPage = () => {
   useEffect(() => {
     const fetchSeatOccupancy = async () => {
       try {
-        const response = await axios.get(`${SERVERIP}/seatmaprouter/seatmap/${showtimeId}/seats`);
+        const response = await axios.get(
+          `${SERVERIP}/seatmaprouter/seatmap/${showtimeId}/seats`
+        );
         setSeatOccupancy(response.data);
         console.log(movie);
         console.log(date1);
         console.log(time1);
-        
       } catch (error) {
         console.error("Error fetching seat occupancy:", error);
       }
@@ -45,8 +46,9 @@ const SeatMapPage = () => {
   }, [showtimeId]);
 
   useEffect(() => {
-    axios.get(`${SERVERIP}/QR/qrData/${paymentId}`)
-      .then(response => {
+    axios
+      .get(`${SERVERIP}/QR/qrData/${paymentId}`)
+      .then((response) => {
         const qrData = response.data;
         if (qrData && !qrData.used) {
           return;
@@ -54,7 +56,7 @@ const SeatMapPage = () => {
           navigate("/QR");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching QR data:", error);
         navigate("/QR");
       });
@@ -63,7 +65,7 @@ const SeatMapPage = () => {
   const sendEmail = async () => {
     try {
       const emailData = {
-        email: localStorage.getItem('loggedInUserEmail'),
+        email: localStorage.getItem("loggedInUserEmail"),
         seatNumber: selectedSeat,
       };
       const response = await axios.post(`${SERVERIP}/QR/sendEmail`, emailData);
@@ -75,7 +77,9 @@ const SeatMapPage = () => {
 
   const handleSeatSelection = (seat) => {
     if (assignedSeat || seatOccupancy[seat]) {
-      setErrorMessage("This seat is already occupied. Please select another seat.");
+      setErrorMessage(
+        "This seat is already occupied. Please select another seat."
+      );
       return;
     }
     setSelectedSeat(seat);
@@ -84,14 +88,21 @@ const SeatMapPage = () => {
 
   const handleConfirmSeat = async () => {
     try {
-      await axios.put(`${SERVERIP}/seatmaprouter/seatmap/${showtimeId}/${selectedSeat}`);
+      await axios.put(
+        `${SERVERIP}/seatmaprouter/seatmap/${showtimeId}/${selectedSeat}`
+      );
       setAssignedSeat(true);
-      await axios.put(`${SERVERIP}/QR/markUsed/${paymentId}`, { showtime: time1, date: date1 });
+      await axios.put(`${SERVERIP}/QR/markUsed/${paymentId}`, {
+        showtime: time1,
+        date: date1,
+      });
 
       // Send email
       await sendEmail();
 
-      setErrorMessage(`The seat ${selectedSeat} is successfully assigned to you. Redirecting to QRs...`);
+      setErrorMessage(
+        `The seat ${selectedSeat} is successfully assigned to you. Redirecting to QRs...`
+      );
       localStorage.setItem("seatassignment", "false");
       setTimeout(() => {
         window.location.href = "/QR";
@@ -108,12 +119,74 @@ const SeatMapPage = () => {
 
   return (
     <div className="seat-booking">
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {assignedSeat && (
+        <div className="flex justify-center items-center h-screen">
+          <h6 className="text-3xl">Assigning seat....</h6>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10"
+            viewBox="0 0 24 24"
+          >
+            <defs>
+              <filter id="svgSpinnersGooeyBalls20">
+                <feGaussianBlur
+                  in="SourceGraphic"
+                  result="y"
+                  stdDeviation={1}
+                ></feGaussianBlur>
+                <feColorMatrix
+                  in="y"
+                  result="z"
+                  values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7"
+                ></feColorMatrix>
+                <feBlend in="SourceGraphic" in2="z"></feBlend>
+              </filter>
+            </defs>
+            <g filter="url(#svgSpinnersGooeyBalls20)">
+              <circle cx={5} cy={12} r={4} fill="currentColor">
+                <animate
+                  attributeName="cx"
+                  calcMode="spline"
+                  dur="2s"
+                  keySplines=".36,.62,.43,.99;.79,0,.58,.57"
+                  repeatCount="indefinite"
+                  values="5;8;5"
+                ></animate>
+              </circle>
+              <circle cx={19} cy={12} r={4} fill="currentColor">
+                <animate
+                  attributeName="cx"
+                  calcMode="spline"
+                  dur="2s"
+                  keySplines=".36,.62,.43,.99;.79,0,.58,.57"
+                  repeatCount="indefinite"
+                  values="19;16;19"
+                ></animate>
+              </circle>
+              <animateTransform
+                attributeName="transform"
+                dur="0.75s"
+                repeatCount="indefinite"
+                type="rotate"
+                values="0 12 12;360 12 12"
+              ></animateTransform>
+            </g>
+          </svg>
+        </div>
+      )}
       {!assignedSeat && (
         <div>
-          <h1 className="text-3xl font-semibold mb-4">Movie Theatre Seat Booking</h1>
+          <h1 className="text-3xl font-semibold mb-4">
+            Movie Theatre Seat Booking
+          </h1>
           <div className="flex justify-center mb-4">
-            <span className="font-semibold text-lg" style={{ marginBottom: '20px' }}>Screen</span>
+            <span
+              className="font-semibold text-lg"
+              style={{ marginBottom: "20px" }}
+            >
+              Screen
+            </span>
           </div>
           <svg
             className="w-3/4 mx-auto mb-4"
@@ -127,20 +200,27 @@ const SeatMapPage = () => {
               strokeWidth="0.3"
             />
           </svg>
-          
           <div className="flex justify-evenly gap-4 ">
             <div className="flex flex-col gap-2">
-              {[...Array(9).keys()].map(row => (
+              {[...Array(9).keys()].map((row) => (
                 <div key={row} className="flex gap-2">
-                  {[...Array(6).keys()].map(col => {
+                  {[...Array(6).keys()].map((col) => {
                     const seatNumber = row * 6 + col + 1;
                     return (
                       <div
                         key={col}
-                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${selectedSeat === seatNumber ? 'bg-green-600' : seatOccupancy[seatNumber] ? 'bg-gray-200' : ''}`}
+                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${
+                          selectedSeat === seatNumber
+                            ? "bg-green-600"
+                            : seatOccupancy[seatNumber]
+                            ? "bg-gray-200"
+                            : ""
+                        }`}
                         onClick={() => handleSeatSelection(seatNumber)}
                         disabled={assignedSeat || seatOccupancy[seatNumber]}
-                        style={{ color: seatOccupancy[seatNumber] ? "red" : "black" }}
+                        style={{
+                          color: seatOccupancy[seatNumber] ? "red" : "black",
+                        }}
                       >
                         <span className="block w-5 h-5">{seatNumber}</span>
                       </div>
@@ -151,17 +231,25 @@ const SeatMapPage = () => {
             </div>
             <div className="w-4"></div> {/* Entrance space */}
             <div className="flex flex-col gap-2">
-              {[...Array(10).keys()].map(row => (
+              {[...Array(10).keys()].map((row) => (
                 <div key={row} className="flex gap-2">
-                  {[...Array(12).keys()].map(col => {
+                  {[...Array(12).keys()].map((col) => {
                     const seatNumber = 54 + row * 12 + col + 1;
                     return (
                       <div
                         key={col}
-                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${selectedSeat === seatNumber ? 'bg-green-600' : seatOccupancy[seatNumber] ? 'bg-gray-200' : ''}`}
+                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${
+                          selectedSeat === seatNumber
+                            ? "bg-green-600"
+                            : seatOccupancy[seatNumber]
+                            ? "bg-gray-200"
+                            : ""
+                        }`}
                         onClick={() => handleSeatSelection(seatNumber)}
                         disabled={assignedSeat || seatOccupancy[seatNumber]}
-                        style={{ color: seatOccupancy[seatNumber] ? "red" : "black" }}
+                        style={{
+                          color: seatOccupancy[seatNumber] ? "red" : "black",
+                        }}
                       >
                         <span className="block w-5 h-5">{seatNumber}</span>
                       </div>
@@ -174,16 +262,24 @@ const SeatMapPage = () => {
             <div className="flex justify-between gap-4 ">
               {/* Container for the 5x5 block */}
               <div className="flex flex-col gap-2">
-                {[...Array(9).keys()].map(row => (
+                {[...Array(9).keys()].map((row) => (
                   <div key={row} className="flex gap-2">
-                    {[...Array(6).keys()].map(col => {
+                    {[...Array(6).keys()].map((col) => {
                       const seatNumber = 174 + row * 6 + col + 1;
                       return (
                         <div
                           key={col}
-                          className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${selectedSeat === seatNumber ? 'bg-green-600' : seatOccupancy[seatNumber] ? 'bg-gray-200' : ''}`}
+                          className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${
+                            selectedSeat === seatNumber
+                              ? "bg-green-600"
+                              : seatOccupancy[seatNumber]
+                              ? "bg-gray-200"
+                              : ""
+                          }`}
                           disabled={assignedSeat || seatOccupancy[seatNumber]}
-                          style={{ color: seatOccupancy[seatNumber] ? "red" : "black" }}
+                          style={{
+                            color: seatOccupancy[seatNumber] ? "red" : "black",
+                          }}
                         >
                           <span className="block w-5 h-5">{seatNumber}</span>
                         </div>
@@ -200,17 +296,25 @@ const SeatMapPage = () => {
           </div>
           <div className="flex justify-evenly gap-4 mb-4">
             <div className="flex flex-col gap-2">
-              {[...Array(7).keys()].map(row => (
+              {[...Array(7).keys()].map((row) => (
                 <div key={row} className="flex gap-2">
-                  {[...Array(7).keys()].map(col => {
+                  {[...Array(7).keys()].map((col) => {
                     const seatNumber = 228 + row * 7 + col + 1;
                     return (
                       <div
                         key={col}
-                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${selectedSeat === seatNumber ? 'bg-green-600' : seatOccupancy[seatNumber] ? 'bg-gray-200' : ''}`}
+                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${
+                          selectedSeat === seatNumber
+                            ? "bg-green-600"
+                            : seatOccupancy[seatNumber]
+                            ? "bg-gray-200"
+                            : ""
+                        }`}
                         onClick={() => handleSeatSelection(seatNumber)}
                         disabled={assignedSeat || seatOccupancy[seatNumber]}
-                        style={{ color: seatOccupancy[seatNumber] ? "red" : "black" }}
+                        style={{
+                          color: seatOccupancy[seatNumber] ? "red" : "black",
+                        }}
                       >
                         <span className="block w-5 h-5">{seatNumber}</span>
                       </div>
@@ -219,20 +323,27 @@ const SeatMapPage = () => {
                 </div>
               ))}
             </div>
-
             <div className="w-4"></div> {/* Entrance space */}
-            <div className="flex flex-col gap-2" style={{ marginTop: '90px' }}>
-              {[...Array(5).keys()].map(row => (
+            <div className="flex flex-col gap-2" style={{ marginTop: "90px" }}>
+              {[...Array(5).keys()].map((row) => (
                 <div key={row} className="flex gap-2">
-                  {[...Array(5).keys()].map(col => {
+                  {[...Array(5).keys()].map((col) => {
                     const seatNumber = 277 + row * 5 + col + 1;
                     return (
                       <div
                         key={col}
-                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${selectedSeat === seatNumber ? 'bg-green-600' : seatOccupancy[seatNumber] ? 'bg-gray-200' : ''}`}
+                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${
+                          selectedSeat === seatNumber
+                            ? "bg-green-600"
+                            : seatOccupancy[seatNumber]
+                            ? "bg-gray-200"
+                            : ""
+                        }`}
                         onClick={() => handleSeatSelection(seatNumber)}
                         disabled={assignedSeat || seatOccupancy[seatNumber]}
-                        style={{ color: seatOccupancy[seatNumber] ? "red" : "black" }}
+                        style={{
+                          color: seatOccupancy[seatNumber] ? "red" : "black",
+                        }}
                       >
                         <span className="block w-5 h-5">{seatNumber}</span>
                       </div>
@@ -241,19 +352,27 @@ const SeatMapPage = () => {
                 </div>
               ))}
             </div>
-            <div className="w-4"></div> 
+            <div className="w-4"></div>
             <div className="flex flex-col gap-2 ">
-              {[...Array(7).keys()].map(row => (
+              {[...Array(7).keys()].map((row) => (
                 <div key={row} className="flex gap-2">
-                  {[...Array(7).keys()].map(col => {
+                  {[...Array(7).keys()].map((col) => {
                     const seatNumber = 332 + row * 7 + col + 1;
                     return (
                       <div
                         key={col}
-                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${selectedSeat === seatNumber ? 'bg-green-600' : seatOccupancy[seatNumber] ? 'bg-gray-200' : ''}`}
+                        className={`seat bg-white-50 border border-gray-400 p-2 text-center cursor-pointer font-roboto text-10 ${
+                          selectedSeat === seatNumber
+                            ? "bg-green-600"
+                            : seatOccupancy[seatNumber]
+                            ? "bg-gray-200"
+                            : ""
+                        }`}
                         onClick={() => handleSeatSelection(seatNumber)}
                         disabled={assignedSeat || seatOccupancy[seatNumber]}
-                        style={{ color: seatOccupancy[seatNumber] ? "red" : "black" }}
+                        style={{
+                          color: seatOccupancy[seatNumber] ? "red" : "black",
+                        }}
                       >
                         <span className="block w-5 h-5">{seatNumber}</span>
                       </div>
@@ -264,9 +383,14 @@ const SeatMapPage = () => {
             </div>
           </div>
           {selectedSeat && (
-            <div className="flex flex-col bg-green-400">
+            <div className="flex flex-col bg-green-300 items-center py-2">
               <p className="text-center">Selected Seat: {selectedSeat}</p>
-              <button onClick={handleConfirmSeat}>Confirm Seat</button>
+              <button
+                onClick={handleConfirmSeat}
+                className="bg-green-500 flex justify-center w-1/6 py-2"
+              >
+                Confirm Seat
+              </button>
             </div>
           )}
         </div>
@@ -276,4 +400,3 @@ const SeatMapPage = () => {
 };
 
 export default SeatMapPage;
-
