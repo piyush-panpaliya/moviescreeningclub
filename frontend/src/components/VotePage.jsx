69d585f2bb443b8d4133b9d3e656aaa67bdb78a4
Lord-Morpheus <tarurak4@gmail.com>
2024-05-03 00:56:22 +0530

    vote page done

--- frontend/src/components/VotePage.jsx
+++ frontend/src/components/VotePage.jsx
@@ -7,7 +7,7 @@ const MovieList = () => {
   const [newMovieData, setNewMovieData] = useState({
     title: "",
     poster: "",
-    genre: ""
+    genre: "",
   });
 
   useEffect(() => {
@@ -61,13 +61,16 @@ const MovieList = () => {
 
   const handleAddMovie = async () => {
     try {
-      const response = await fetch("http://localhost:8000/voterouter/addvotemovie", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newMovieData),
-      });
+      const response = await fetch(
+        "http://localhost:8000/voterouter/addvotemovie",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newMovieData),
+        }
+      );
       if (!response.ok) {
         throw new Error("Failed to add movie");
       }
@@ -75,7 +78,7 @@ const MovieList = () => {
       setNewMovieData({
         title: "",
         poster: "",
-        genre: ""
+        genre: "",
       });
       // Refresh movies after adding
       fetchMovies();
@@ -87,9 +90,12 @@ const MovieList = () => {
   const handleDeleteMovie = async (movieId) => {
     console.log("deleting");
     try {
-      const response = await fetch(`http://localhost:8000/voterouter/deletevotemovie/${movieId}`, {
-        method: "DELETE",
-      });
+      const response = await fetch(
+        `http://localhost:8000/voterouter/deletevotemovie/${movieId}`,
+        {
+          method: "DELETE",
+        }
+      );
       if (!response.ok) {
         throw new Error("Failed to delete movie");
       }
@@ -100,104 +106,168 @@ const MovieList = () => {
     }
   };
   const MovieCard = ({ movie }) => {
-
     const totalVotes = movie.yesCount + movie.noCount;
-    const yesPercentage = totalVotes === 0 ? 0 : (movie.yesCount / totalVotes) * 100;
-    const noPercentage = totalVotes === 0 ? 0 : (movie.noCount / totalVotes) * 100;
+    const yesPercentage =
+      totalVotes === 0 ? 0 : (movie.yesCount / totalVotes) * 100;
+    const noPercentage =
+      totalVotes === 0 ? 0 : (movie.noCount / totalVotes) * 100;
     console.log(movie._id);
     return (
-      <div className="flex bg-white rounded-lg shadow-md p-4 m-4 w-full relative">
-        <div className="w-full h-full object-cover">
-          <div className="w-48 h-auto mb-2 rounded mr-4">
+      <div className="flex bg-white rounded-lg shadow-md w-full h-full">
+        <div className="flex flex-col w-2/3 ml-2">
+          <div className="w-2/3 mb-2 rounded">
             <img
-              className="object-cover w-full h-full rounded-md"
+              className="w-2/3  rounded-md"
               src={movie.poster}
               alt={movie.title}
             />
           </div>
           <div className="flex flex-col mt-1">
-            <p className="font-semibold text-2xl max-sm:text-lg">
+            <p className="font-semibold text-2xl max-lg:text-base">
               {movie.title}
             </p>
             <p className="max-sm:text-sm">{movie.genre}</p>
           </div>
         </div>
-  
-        <div className="absolute top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center">
-          <div className="flex flex-col items-center mb-2">
-            <div className="flex flex-row">
-              <span className="font-semibold text-gray-600 px-2">Yes</span>
-              <div className="bg-blue-200 h-8 rounded-xl w-40">
-                <div
-                  className="bg-blue-500 h-8 rounded-xl"
-                  style={{ width: `${yesPercentage}%` }}
-                ></div>
+        <div className="flex flex-col justify-between mr-2 w-full">
+          <div className="flex justify-end">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="red"
+              className="w-6 h-6 cursor-pointer"
+              onClick={handleDeleteMovie}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center mb-2">
+              <div className="flex flex-row ">
+                {/* <span className="font-semibold text-gray-600 px-2">Yes</span> */}
+                <div className="bg-green-200 h-8 rounded-xl w-60 max-lg:w-40 max-sm:w-32 text-center">
+                  <div
+                    className="bg-green-500 h-8 rounded-xl"
+                    style={{ width: `${yesPercentage}%` }}
+                  >
+                    YES
+                  </div>
+                </div>
+                {!movie.voters.includes(userEmail) && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="green"
+                    className="w-8 h-8"
+                    onClick={() => handleVoteClick(movie._id, "yes")}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                )}
               </div>
-            </div>
-            <div className="flex flex-row pt-2">
-              <span className="font-semibold text-gray-600 px-2">No</span>
-              <div className="bg-red-200 h-8 rounded-xl  w-40">
-                <div
-                  className="bg-red-500 h-8 rounded-xl"
-                  style={{ width: `${noPercentage}%` }}
-                ></div>
+              <div className="flex flex-row pt-2">
+                {/* <span className="font-semibold text-gray-600 px-2">No</span> */}
+                <div className="bg-red-200 h-8 rounded-xl w-60 max-lg:w-40 max-sm:w-32 text-center">
+                  <div
+                    className="bg-red-500 h-8 rounded-xl"
+                    style={{ width: `${noPercentage}%` }}
+                  >
+                    NO
+                  </div>
+                </div>
+                {!movie.voters.includes(userEmail) && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="red"
+                    className="w-8 h-8 "
+                    onClick={() => handleVoteClick(movie._id, "no")}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                )}
               </div>
             </div>
           </div>
-        </div>
-  
-        <div className="absolute bottom-0 right-0">
-          {!movie.voters.includes(userEmail) && (
-            <div className="flex flex-col items-end">
-              <button
-                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 mr-2"
-                onClick={() => handleVoteClick(movie._id, "yes")}
-              >
-                Vote Yes
-              </button>
-              <button
-                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-                onClick={() => handleVoteClick(movie._id, "no")}
-              >
-                Vote No
-              </button>
-            </div>
-          )}
         </div>
       </div>
     );
   };
   return (
-    <div className="flex flex-wrap gap-4 justify-evenly">
-      {/* Render existing movies */}
-      {movies.map((movie) => (
-        <div key={movie._id} className="w-full  lg:w-1/3">
-          <MovieCard movie={movie} />
-         
+    <div className="flex flex-col items-center justify-center font-monts">
+      <div className="text-2xl my-3 capitalize font-bold">vote the movies you want to watch next</div>
+      <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-10 w-[95%] my-6">
+        {/* Render existing movies */}
+        {movies.map((movie) => (
+          <div key={movie._id} className="w-full h-full">
+            <MovieCard movie={movie} />
+          </div>
+        ))}
+
+        <div className="flex flex-col items-center rounded-lg shadow-md w-full gap-2 py-2">
+          <h2 className="font-semibold text-lg">Add New Movie</h2>
+          <input
+            type="text"
+            placeholder="Title"
+            value={newMovieData.title}
+            className="border-2 border-black rounded-md"
+            onChange={(e) =>
+              setNewMovieData({ ...newMovieData, title: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Poster URL"
+            value={newMovieData.poster}
+            className="border-2 border-black rounded-md"
+            onChange={(e) =>
+              setNewMovieData({ ...newMovieData, poster: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Genre"
+            value={newMovieData.genre}
+            className="border-2 border-gray-950 rounded-md"
+            onChange={(e) =>
+              setNewMovieData({ ...newMovieData, genre: e.target.value })
+            }
+          />
+          <button onClick={handleAddMovie}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="green"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </button>
         </div>
-      ))}
-      
-      <div className="flex bg-white rounded-lg shadow-md p-4 m-4 w-full relative">
-        <h2>Add New Movie</h2>
-        <input 
-          type="text" 
-          placeholder="Title" 
-          value={newMovieData.title} 
-          onChange={(e) => setNewMovieData({ ...newMovieData, title: e.target.value })} 
-        />
-        <input 
-          type="text" 
-          placeholder="Poster URL" 
-          value={newMovieData.poster} 
-          onChange={(e) => setNewMovieData({ ...newMovieData, poster: e.target.value })} 
-        />
-        <input 
-          type="text" 
-          placeholder="Genre" 
-          value={newMovieData.genre} 
-          onChange={(e) => setNewMovieData({ ...newMovieData, genre: e.target.value })} 
-        />
-        <button onClick={handleAddMovie}>Add Movie</button>
       </div>
     </div>
   );
