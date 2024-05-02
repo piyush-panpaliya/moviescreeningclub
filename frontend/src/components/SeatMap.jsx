69d088ba217f9adbfe5c95122e2166a8d88c8c61
Aryan <aryanjain2462005@gmail.com>
2024-05-03 02:50:22 +0530

    seatmappage bug fixed

--- frontend/src/components/SeatMap.jsx
+++ frontend/src/components/SeatMap.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { getToken } from "../utils/getToken";
 import { useLocation, useNavigate } from "react-router-dom";
 import moment from "moment";
 import { SERVERIP } from "../config";
