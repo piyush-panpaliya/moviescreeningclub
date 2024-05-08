9a606b30f944680df59d23f84c455625c665c903
Lord-Morpheus <tarurak4@gmail.com>
2024-05-08 16:04:51 +0530

    route fixed

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -3,5 +3,5 @@ const paymentController = require('../controllers/payment.controller');
 const router = express.Router();
 
 router.post('/checkPayment', paymentController.check);
-router.post('/tempPayment', paymentController.temp);
+router.post('/tempPayment', paymentController.saveTempPayment);
 module.exports = router;
