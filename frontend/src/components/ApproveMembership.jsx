fa674637ff79bb0ed605120c1ea2fd2060cef0db
Lord-Morpheus <tarurak4@gmail.com>
2024-05-08 16:33:13 +0530

    approve  membership page added

--- frontend/src/components/ApproveMembership.jsx
+++ frontend/src/components/ApproveMembership.jsx
@@ -1,42 +1,45 @@
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
 
 export default function ApproveMembership() {
-  const columns = ["name", "email", "designation", "Role", "actions"];
+  const columns = ["name", "email", "phone Number", "designation",'transaction id',"actions"];
   return (
     <div>
       <h1>Approve Membership</h1>
       <div className="flex justify-center min-h-lvh">
-      <Table
-        isStriped
-        className="w-4/5 max-sm:w-[95%] my-5"
-        aria-label="Controlled table example with dynamic content"
-        topContent={topContent}
-        topContentPlacement="outside"
-      >
-        <TableHeader
-          className="capitalize"
-          columns={columns.map((column) => ({ key: column, label: column }))}
+        <Table
+          isStriped
+          className="w-5/6 max-sm:w-[95%] my-5"
+          aria-label="Example static collection table"
         >
-          {(column) => (
-            <TableColumn className="capitalize" key={column.key}>
-              {column.label}
-            </TableColumn>
-          )}
-        </TableHeader>
-        <TableBody items={filteredUsers}>
-          {(item) => (
-            <TableRow key={item._id}>
-              {columns.map((columnKey) => (
-                <TableCell key={columnKey}>
-                  {getKeyValue(item, columnKey)}
-                </TableCell>
-              ))}
+          <TableHeader
+            className="capitalize"
+            columns={columns.map((column) => ({ key: column, label: column }))}
+          >
+            {(column) => (
+              <TableColumn className="capitalize" key={column.key}>
+                {column.label}
+              </TableColumn>
+            )}
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>Tony Reichert</TableCell>
+              <TableCell>CEO</TableCell>
+              <TableCell>Active</TableCell>
+              <TableCell>Active</TableCell>
+              <TableCell>Active</TableCell>
+              <TableCell>Active</TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
